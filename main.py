d7400e9c30d14be4927f7786d3c3c122bfd6286e
Melanie Jaimes <113930529+majaimes1@users.noreply.github.com>
2022-10-14 17:21:53 +0000

    used choice and random

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
 from random import shuffle
+from random import choice
 # random
   # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
   #show you two useful functions for now.
@@ -32,4 +33,8 @@ print(num)
 if num % 2 == 0:
   print("number is even")
 else: print("number is odd")
+
+color = ["red", "blue", "pink", "purple"]
+random_color = choice(color)
+print(f"random color is {random_color}")
   
